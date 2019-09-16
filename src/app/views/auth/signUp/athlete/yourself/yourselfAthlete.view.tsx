716c26a18163e0c87abf80acefbfb0e1b20e280b
Mikhail Poliakov <poliakov895@gmail.com>
2019-09-16 16:43:40 +0300

    merge

--- src/app/views/auth/signUp/athlete/yourself/yourselfAthlete.view.tsx
+++ src/app/views/auth/signUp/athlete/yourself/yourselfAthlete.view.tsx
@@ -31,7 +31,7 @@ class YourSelfAthleteScreen extends Component<Props, State> {
             isDateTimePickerVisible: false,
             birthDate: '',
             birthDateError: false,
-            genderError: true,
+            genderError: false,
         }
     }
 
@@ -44,7 +44,7 @@ class YourSelfAthleteScreen extends Component<Props, State> {
     };
 
     public handleDatePicked = (date: Date) => {
-   const formattedDate = moment(date).format('DD-MM-YYYY');
+        const formattedDate = moment(date).format('DD-MM-YYYY');
 
         const birthDateError = this.birthDateValidation(formattedDate);
 
