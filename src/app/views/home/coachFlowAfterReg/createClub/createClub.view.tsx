eb44f1202c8e8042d865c06b81dac596b81f75ae
Anuitexdev <anuitex@gmail.com>
2019-10-07 11:20:59 +0300

    chart

--- src/app/views/home/coachFlowAfterReg/createClub/createClub.view.tsx
+++ src/app/views/home/coachFlowAfterReg/createClub/createClub.view.tsx
@@ -4,9 +4,11 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import createClubStyle from "./createClub.style";
 import { TextInput, ScrollView } from "react-native-gesture-handler";
 import * as actions from '../../../../redux/actions/createGroup.actions';
-import { Scope } from "@babel/traverse";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
 interface State {
     placeholder: string;
+
 }
 
 interface Props {
@@ -14,14 +16,25 @@ interface Props {
 }
 
 class CreateClubView extends Component<Props, State> {
-
+    public chart:any;
     constructor(props: Props) {
         super(props)
 
         this.state = {
-            placeholder: ''
+            placeholder: '',
         }
     }
+
+    componentDidMount() {
+        let chart =  am4core.create("sdsd",am4charts.Axis);
+        this.chart = chart;
+    }
+
+    componentWillUnmount() {
+        if (this.chart) {
+          this.chart.dispose();
+        }
+      }
 
     public onSubmit() {
         this.props.nextStepNumber(2)
@@ -42,6 +55,10 @@ class CreateClubView extends Component<Props, State> {
                        value={this.state.placeholder}
                        >
                        </TextInput>
+                       <View>
+                            <Text>dsfsdf</Text>  
+                            {this.chart}                      
+                       </View>
                        <TouchableOpacity 
                        style={createClubStyle.nextBtn}
                        onPress={() => this.onSubmit()}
