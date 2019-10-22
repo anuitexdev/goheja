dadad67f8f97f8c36c422c49fdb6b06e0ea2252f
Mikhail Poliakov <poliakov895@gmail.com>
2019-10-22 12:47:46 +0300

    add service for worker data

---
+++ src/app/services/workout.service.ts
@@ -0,0 +1,25 @@
+import axiosInstance from '../shared/interceptors/axios.interceptor';
+import Config from 'react-native-config';
+
+export class WorkoutService {
+
+    public getChartData(): Promise<any> {
+        const testData: any = {
+            workoutId:"5da6d55955ec451b68fc0ae3", 
+            userId:"5b96853cdd6505dd6ba78a8c",
+            specGroup:"TestEnv"
+           }
+        return axiosInstance.get(`${Config.API_URL}WorkOuts`, testData)
+                .then((response) => {
+                    console.log(response)
+                    return response
+                })
+                .catch((error) => {
+                    console.log(error)
+                    return error
+                })
+    }
+    
+}
+
+export default new WorkoutService();
